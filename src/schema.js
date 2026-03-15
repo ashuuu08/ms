1dcec90a3212a019555ed19c046b787fa2aac83b
ashish rathour <rajpootashishd@gmail.com>
2026-03-16 03:28:08 +0530

    Add robots.txt, sitemap.xml, and organization schema for SEO optimization

---
+++ src/schema.js
@@ -0,0 +1,11 @@
+export const organizationSchema = {
+ "@context": "https://schema.org",
+ "@type": "Organization",
+ "name": "Ashbit Soft",
+ "url": "https://ashbit.in",
+ "address": {
+   "@type": "PostalAddress",
+   "addressLocality": "Anuppur",
+   "addressCountry": "India"
+ }
+};
